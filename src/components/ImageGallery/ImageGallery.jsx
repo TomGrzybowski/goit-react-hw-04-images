ac4ca5122df3367eea1ba5bc8b09cb7429815503
TomekGrzybek <t.grzybowski94@gmail.com>
2023-04-01 20:44:18 +0200

    remove commented code

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -21,31 +21,5 @@ ImageGallery.propTypes = {
   galleryItems: PropTypes.array,
   pages: PropTypes.number,
 };
-// class ImageGallery extends Component {
-//   static defaultProps = {
-//     galleryItems: [],
-//     pages: 0,
-//   };
-
-//   static propTypes = {
-//     galleryItems: PropTypes.array,
-//     pages: PropTypes.number,
-//   };
-
-//   render() {
-//     return (
-//       <ul className="image-gallery" onClick={this.props.handleImageClick}>
-//         {this.props.galleryItems.map(image => (
-//           <ImageGalleryItem
-//             key={image.id}
-//             id={image.id}
-//             src={image.webformatURL}
-//             alt={image.tags}
-//           />
-//         ))}
-//       </ul>
-//     );
-//   }
-// }
 
 export default ImageGallery;
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -29,35 +29,4 @@ Modal.propTypes = {
   alt: PropTypes.string,
 };
 
-// class Modal extends Component {
-//   static propTypes = {
-//     escFunction: PropTypes.func,
-//     src: PropTypes.string,
-//     alt: PropTypes.string,
-//   };
-
-//   componentDidMount() {
-//     document.addEventListener('keydown', this.props.escFunction);
-//     const overlay = document.querySelector('.overlay');
-
-//     overlay.addEventListener('click', this.props.handleOverlayClick);
-//   }
-
-//   componentWillUnmount() {
-//     document.removeEventListener('keydown', this.props.escFunction);
-//     const overlay = document.querySelector('.overlay');
-//     overlay.removeEventListener('click', this.props.handleOverlayClick);
-//   }
-
-//   render() {
-//     return (
-//       <div className="overlay">
-//         <div className="modal">
-//           <img src={this.props.src} alt={this.props.alt} />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
 export default Modal;
